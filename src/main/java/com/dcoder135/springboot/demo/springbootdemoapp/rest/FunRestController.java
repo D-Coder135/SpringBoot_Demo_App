b2751cd5d6126a8f6d766e5b477e605b7be7311e
D-Coder135 <devansh1352000@gmail.com>
2023-03-24 15:25:21 +0530

    add an annotation GetMapping("/")

--- src/main/java/com/dcoder135/springboot/demo/springbootdemoapp/rest/FunRestController.java
+++ src/main/java/com/dcoder135/springboot/demo/springbootdemoapp/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.dcoder135.springboot.demo.springbootdemoapp.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -7,4 +8,5 @@ public class FunRestController {
 
 //    expose "/" that return "Hello World"
 
+    @GetMapping("/")
 }
