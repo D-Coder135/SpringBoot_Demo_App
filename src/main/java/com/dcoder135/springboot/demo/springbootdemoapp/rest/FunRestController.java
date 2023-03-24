59820dd81519e08be0808d18ac1ef1e8dc988dde
D-Coder135 <devansh1352000@gmail.com>
2023-03-24 15:24:47 +0530

    add the annotation @RestController

--- src/main/java/com/dcoder135/springboot/demo/springbootdemoapp/rest/FunRestController.java
+++ src/main/java/com/dcoder135/springboot/demo/springbootdemoapp/rest/FunRestController.java
@@ -1,5 +1,8 @@
 package com.dcoder135.springboot.demo.springbootdemoapp.rest;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FunRestController {
 
 //    expose "/" that return "Hello World"
